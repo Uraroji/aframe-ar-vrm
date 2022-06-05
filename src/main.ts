7731bae64021efafb3288c8c75566de088a8feff
SuzukiDaishi <zukky.rikugame@gmail.com>
2022-06-06 00:20:45 +0900

    first commit

--- src/main.ts
+++ src/main.ts
@@ -28,19 +28,21 @@ AFRAME.registerComponent('vrm-model', {
           this.el.emit('vrm-error', {format: 'vrm', src: this.data});
         }
 
-      //   try {
-      //     if (this.vrm) {
-      //       const gltf = await gltfLoader.loadAsync('assets/walk.glb')
-      //       const walkClip = mixamoClipToVRMClip(gltf.animations[0], this.vrm, false)
-      //       walkClip.name = 'walk'
-      //       this.mixer = new AFRAME.THREE.AnimationMixer(this.vrm.scene)
-      //       this.walk = this.mixer.clipAction(walkClip).setEffectiveWeight(1.0)
-      //       this.walk.play()
-      //       this.vrm.scene.rotateX(-Math.PI/2)
-      //     }
-      //   } catch(error) {
-      //     console.log(error)
-      //   }
+        try {
+          if (this.vrm) {
+            const gltf = await gltfLoader.loadAsync('assets/dance.glb')
+            const walkClip = mixamoClipToVRMClip(gltf.animations[0], this.vrm, false)
+            walkClip.name = 'walk'
+            this.mixer = new AFRAME.THREE.AnimationMixer(this.vrm.scene)
+            this.walk = this.mixer.clipAction(walkClip).setEffectiveWeight(1.0)
+            this.walk.play()
+            this.vrm.scene.rotateX(-Math.PI/2)
+            // this.vrm.scene.position.z += 1
+            // this.vrm.scene.position.y += 1
+          }
+        } catch(error) {
+          console.log(error)
+        }
       })()
     }
   },
