680f5f9d0c989db2369decc87cd1cf3a4c7c479c
SuzukiDaishi <zukky.rikugame@gmail.com>
2022-06-07 03:42:44 +0900

    fix Readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # aframe-ar-vrm
+
+## 目標
+- [A-FRAME](https://aframe.io/docs/1.3.0/introduction/)開発のベストプラクティスを見つける
+    - 独自のコンポーネントシステムを採用している
+    - 過去の遺産が割とあるのでこれを活用したい AR.jsなど
+    - 最低限TypeScriptを使いたい
+    - ReactやVue風に書きたい
+- [VRM](https://vrm.dev/)を扱いたい
+    - [binzume/aframe-vrm](https://github.com/binzume/aframe-vrm)はあるけど，[@pixiv/three-vrm](https://github.com/pixiv/three-vrm)ベースで書き換えたい
+    - ローカルファイルのVRMとかVRoidHubとかからファイルを持ってきたい
+        - [この辺](https://github.com/SuzukiDaishi/peppersghost-vrm/blob/main/src/UserLoader.ts)参考に
+    - 動かしたい
+        - [この辺](./src/VRMAnimationClip.ts)参考に
+- メタバースしたい
+    - VRMのコントローラを作る
+        - キーボード
+        - VRコントローラ
+        - スマホ
+    - VRWebサイト間のシームレスな移動を可能にするハイパーリンクを作りたい
+        - モデル情報や諸々の情報を移行できる
+        - [この辺](https://github.com/Uraroji/three-vrm-send)参考に
--- src/main.ts
+++ src/main.ts
@@ -28,27 +28,33 @@ AFRAME.registerComponent('vrm-model', {
           this.el.emit('vrm-error', {format: 'vrm', src: this.data});
         }
 
-        // try {
-        //   if (this.vrm) {
-        //     const gltf = await gltfLoader.loadAsync('assets/dance.glb')
-        //     const walkClip = mixamoClipToVRMClip(gltf.animations[0], this.vrm, false)
-        //     walkClip.name = 'walk'
-        //     this.mixer = new AFRAME.THREE.AnimationMixer(this.vrm.scene)
-        //     this.walk = this.mixer.clipAction(walkClip).setEffectiveWeight(1.0)
-        //     this.walk.play()
-        //     this.vrm.scene.rotateX(-Math.PI/2)
-        //     this.vrm.scene.position.z += 1
-        //     this.vrm.scene.position.y += 1
-        //   }
-        // } catch(error) {
-        //   console.log(error)
-        // }
+        try {
+          if (this.vrm) {
+            const gltf = await gltfLoader.loadAsync('assets/dance.glb')
+            const walkClip = mixamoClipToVRMClip(gltf.animations[0], this.vrm, false)
+            walkClip.name = 'walk'
+            this.mixer = new AFRAME.THREE.AnimationMixer(this.vrm.scene)
+            this.walk = this.mixer.clipAction(walkClip).setEffectiveWeight(1.0)
+            this.walk.play()
+
+            // アニメーション適用時
+            this.el.object3D.rotateX(-Math.PI/2)
+            this.el.object3D.position.z += 1
+            this.el.object3D.position.y += 1
+          }
+        } catch(error) {
+          console.log(error)
+        }
+
       })()
     }
   },
 
   tick(time, timeDelta) {
+
+    console.log(this.el.object3D.position);
     
+
     if (this.mixer) {
       this.mixer.update(timeDelta)
     }
